c06d53d6f71d78af7dd652f0916b49ed9c7d7942
Kim Nguyen <kimnivore@gmail.com>
2022-07-26 08:19:23 -0700

    day 3 part 1;

--- 2021/day03.js
+++ 2021/day03.js
@@ -5,4 +5,39 @@ const array = fs
   .split("\n")
   .filter((x) => Boolean(x));
 
-  console.log(array)
+ let length = array[0].length;
+
+ function count(array) {
+  const zeroes = Array(length).fill(0);
+  const ones = Array(length).fill(0);
+
+  for (const item of array) {
+    const bits = [...item];
+    bits.forEach((bit, index) => {
+      if (bit === "0") {
+        zeroes[index]++;
+      } else {
+        ones[index]++;
+      }
+    });
+  }
+  return { zeroes, ones};
+ }
+
+  function consumption() {
+    const { zeroes, ones } = count(array);
+    let gamma = '';
+    let epsilon = '';
+
+    for(let i = 0; i < length; i++) {
+      let bit = 0;
+      if(ones[i] > zeroes[i]) {
+        bit = 1;
+      }
+      gamma+=bit;
+      epsilon+=bit === 1 ? 0 : 1;
+    }
+    console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
+  }
+ consumption();
+
